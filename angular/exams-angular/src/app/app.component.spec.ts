224db1e30e8237cecfa5c4a798c3eb871eda7651
EduarSolaez <eduarsoli703@gmail.com>
2024-02-13 19:47:24 -0500

    Using new project angular called exams-angular but with module.ts

--- angular/exams-angular/src/app/app.component.spec.ts
+++ angular/exams-angular/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'exams' title`, () => {
+  it(`should have as title 'exams-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('exams');
+    expect(app.title).toEqual('exams-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, exams');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, exams-angular');
   });
 });
