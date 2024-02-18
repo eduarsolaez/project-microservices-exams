f67eb1a2471971e819819fad5cab5f1a76ea8a32
EduarSolaez <eduarsoli703@gmail.com>
2024-02-18 00:58:06 -0500

    update

--- microservice-users/src/main/java/com/eduar/microservices/app/users/controllers/StudentController.java
+++ microservice-users/src/main/java/com/eduar/microservices/app/users/controllers/StudentController.java
@@ -19,6 +19,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("api/students")
+//@CrossOrigin({"http://localhost:4200/"})
 public class StudentController extends CommonController<StudentEntity, IStudentService> {
 
     @PutMapping("{id}")
